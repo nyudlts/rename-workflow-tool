1bcfc2b27625f05c0d0d4e2f062ed1a027931539
Don Mennerich <don.mennerich@nyu.edu>
2025-07-18 14:51:40 -0400

    adding cobra scaffolding

--- cmd/root.go
+++ cmd/root.go
@@ -1,1 +1,21 @@
 package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Welcome to the AV Processor CLI!")
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "github.com/nyudlts/av-processor/cmd"
 
+func main() {
+	cmd.Execute()
 }
